3c56f07a9890375fe1506aa1254f2240e54b9961
KID <50666207+KID421@users.noreply.github.com>
2019-12-18 14:25:08 +0800

    認識 API 並使用插值取得兩個點之間的位置

---
+++ UnityProject/Assets/Scripts/LearnLerp.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class LearnLerp : MonoBehaviour
+{
+    public Transform cube, sphere;
+
+    private void Start()
+    {
+        Vector3 pos = Vector3.Lerp(cube.position, sphere.position, 0.5f);
+        print(pos);
+    }
+}
